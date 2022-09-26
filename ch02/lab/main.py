d60bb7ce3626e70664479699b2a22027b7435b3b
Cody Musial <112524040+codymusial@users.noreply.github.com>
2022-09-26 21:23:47 +0000

    CH2 Lab

--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -1,16 +1,18 @@
-
+import random
 #Part A
 weeks = 16
 classes = 5
 tuition = 6000
-classes_per_week = 3
+classes_per_week = 2
 
 cost_per_week = ((tuition / classes) / weeks)
 
 cost_per_class = (cost_per_week / classes_per_week)
 
-print("Cost per week:", cost_per_week)
+print("Cost per week:", str(cost_per_week))
 
-print("Cost per class:", cost_per_class)
+print("Cost per class:", str(cost_per_class))
 
 #Part B
+list = [1,2,3,4,5]
+print("random choice: " + str(random.choice(list)))
