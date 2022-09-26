62ea6e055ef119f4046230e687b072a62d4ab000
Cody Musial <112524040+codymusial@users.noreply.github.com>
2022-09-26 21:07:30 +0000

    CH2 Exercise MyTurtle Done

--- ch02/exercises/myturtle.py
+++ ch02/exercises/myturtle.py
@@ -2,15 +2,16 @@ import turtle
 window = turtle.Screen()
 my_turtle = turtle.Turtle()
 my_turtle.shape('turtle')
-my_turtle.color("green")
+my_turtle.color("purple")
+for i in [1,2,3,4]:
+  my_turtle.fd(50)
+  my_turtle.right(90)
 
-
-turtle.fd(50)
-turtle.right(90)
-turtle.fd(50)
-turtle.right(90)
-turtle.fd(50)
-turtle.right(90)
-turtle.fd(50)
-
+my_turtle.color('red')
+my_turtle.penup()
+my_turtle.setpos(-75,0)
+my_turtle.pendown()
+for i in [1,2,3,4]:
+  my_turtle.fd(50)
+  my_turtle.right(90)
 window.exitonclick()
